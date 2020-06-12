6693bdbe18eb7aeaffe0bbd94f5fa1f344f28237
Parth Sihora <Parth.Sihora@ey.com>
2020-06-12 09:59:07 -0700

    Add project files.

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net.Http.Headers;
+using Splitio.Services.Client.Classes;
+using System;
+
+namespace Angular5FileUpload.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class UploadController : Controller
+    {
+        private IHostingEnvironment _hostingEnvironment;
+
+        public UploadController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpPost, DisableRequestSizeLimit]
+        public ActionResult UploadFile()
+        {
+            var config = new ConfigurationOptions();
+
+            var factory = new SplitFactory("v0gs87g1pd0frk07c62eqdr1kkhebo05bkne", config);
+            var splitClient = factory.Client();
+            try
+            {
+                splitClient.BlockUntilReady(10000);
+            }
+            catch (Exception)
+            {
+                // log & handle
+            }
+
+            var treatment = splitClient.GetTreatment("parth","testsplit");
+            if (treatment == "on")
+            {
+                var file = Request.Form.Files[0];
+                string folderName = "Upload";
+                string webRootPath = _hostingEnvironment.WebRootPath;
+                string newPath = Path.Combine(webRootPath, folderName);
+                if (!Directory.Exists(newPath))
+                {
+                    Directory.CreateDirectory(newPath);
+                }
+                if (file.Length > 0)
+                {
+                    string fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    string fullPath = Path.Combine(newPath, fileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+                return Json("Upload Successful.");
+            }
+            else if (treatment == "off")
+            {
+                return Json("You do not have permission to upload.");
+            }
+            else
+                return Json("There was error");
+            }
+        }
+    }
